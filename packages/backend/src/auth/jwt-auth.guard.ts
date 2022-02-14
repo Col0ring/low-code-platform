fd2487da5b1a5d172ddf486f959ab76ef2af37ad
Col0ring <1561999073@qq.com>
2022-02-15 02:06:48 +0800

    feat: add public decorator

--- packages/backend/src/auth/jwt-auth.guard.ts
+++ packages/backend/src/auth/jwt-auth.guard.ts
@@ -1,9 +1,30 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
 import { AuthGuard } from '@nestjs/passport'
-import { StrategyType } from './constants'
+import { DecoratorMetadata, StrategyType } from './constants'
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard(StrategyType.Jwt) {
+  constructor(private readonly reflector: Reflector) {
+    super()
+  }
+
+  override canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride(
+      DecoratorMetadata.Public,
+      [context.getHandler(), context.getClass()]
+    )
+
+    if (isPublic) {
+      return true
+    }
+    return super.canActivate(context)
+  }
+
   override handleRequest(err: any, user: any, info: any) {
     if (err || !user) {
       throw err || new UnauthorizedException(info?.message)
